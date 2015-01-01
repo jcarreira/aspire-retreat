a541290297c8bd8aa6fc28946eebc4eb1ab55716
Joao Carreira <joao@berkeley.edu>
2015-01-01 01:56:25 +0100

    New data

--- ramcloud/plot_lat.r
+++ ramcloud/plot_lat.r
@@ -1,26 +1,40 @@
+# Script to generate graph for Aspire Retreat
+# Read latency of RAMCloud's ClusterPerf Benchmark in FireboxV0 vs Ramcloud's cluster
+#  
+# Benchmark workflow:
+# 1. Coordinator and Master are launched
+# 2. For each size (100 bytes to 1MB):
+# 2.1 Fill single table with 500MB of key-values data
+# 2.2 Read benchmark:
+# 2.2.1 Read 100K times synchronously a random key
+# 2.3 Write benchmark
+# 2.3.1 Write 100k times synchronously a random key
 
 pdf("ramcloud_benchmark_lat.pdf")
 par(mar=c(8, 4, 4, 2) + 0.2)
 
 
-data <- matrix(c(10.9, 5.4,
-             10.9, 7.7,
-             11.2, 11.1,
-             29.4, 44,
-            181.2, 363,
-             10.8, 16.2,
-             10.9, 20.8,
-             12.4, 37.7,
-             67.7, 311,
-            611.9, 2300), ncol=10, nrow=2)
+data <- matrix(c(3.0, 5.4,
+             4.0, 7.7,
+             5.6, 11.1,
+             21.8, 44,
+            171.3, 363,
+             4.2, 16.2,
+             5.4, 20.8,
+             10.1, 37.7,
+             59.8, 311,
+            594.6, 2300), ncol=10, nrow=2)
 
 par(las=2)
 
-labs <- c("Read-100", "Read-1K", "Read-10K", "Read-100K", "Read-1M", "Write-100", "Write-1k", "Write-10K", "Write-100K", "Write-1M")
-x <- barplot(data, main="Ramcloud Latency Benchmark (90th percentile)",
+labs <- c("Basic.Read-100B", "Basic.Read-1K", "Basic.Read-10K", "Basic.Read-100K", "Basic.Read-1M", "Basic.Write-100B", "Basic.Write-1k", "Basic.Write-10K", "Basic.Write-100K", "Basic.Write-1M")
+x <- barplot(data, main="Ramcloud Median Latency (90th percentile)",
         ylab="Latency (us)", xlab="",
         col=c("darkblue","darkgreen"), 
         beside=TRUE,
         names.arg=labs,
+        log="y",
         legend=c("Firebox", "Ramcloud HW"))
+
+axis(side = 2, at = c(0,1,2,3))
 #text(cex=1, x=x-0.25, y=-10, labs, xpd=TRUE, srt=45)
